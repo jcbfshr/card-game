92313612ad7440ca228255d7916f0a66da948601
Jacob Fisher <48258052+jcbfshr@users.noreply.github.com>
2020-04-23 15:55:16 +0100

    v1.1.0
    - added leaderboard functionality
    - code reformatting

--- cardGame.py
+++ cardGame.py
@@ -1,7 +1,7 @@
 # import dependencies
 import algo,unittest
 
-# initialise variables
+# initialise global deck and card value possibilities
 suits = ["red","black","yellow"]
 faces = [x for x in range(1, 11)]
 deck = []
@@ -12,21 +12,31 @@ class Player:
         self.name = name
         self.deck = []
     
-    def take_card(self,origin):
-        algo.move(origin,self.deck)
+    # move i cards from origin to object deck
+    def take_card(self,origin,i=1):
+        algo.move(origin,self.deck,i)
 
 # create deck of cards and shuffle
 def prepare():
+    # make deck based on given suits and faces
     for i in range(len(suits)):
         for x in range(len(faces)):
             deck.append([suits[i],faces[x]])
+
     algo.shuffle(deck)
     return deck
 
+# return winner based on each player's card
 def round_winner(card1,card2):
-    if card1[0] == card2[0]:
+    if card1[0] == card2[0]: # if suits are equal
         return "p1" if card1[1] > card2[1] else "p2"
     else:
+        ''' determine winner based on table if suits unequal:
+        CARD     CARD     WINNER
+        Red      Black    Red
+        Yellow   Red      Yellow
+        Black    Yellow   Black
+        '''
         for i in range(2):
             if card1[0] == "red" and card2[0] == "black":
                 return "p1" if i == 0 else "p2"
@@ -37,20 +47,34 @@ def round_winner(card1,card2):
             # reverse order of cards to find winner if not already found
             card1,card2 = card2,card1
 
+# return winner of game based on number of cards
 def game_winner(deck1,deck2):
     return "p1" if len(deck1) > len(deck2) else "p2"
 
+# run full sequence
 def play(name1,name2):
+    # setup player objects
     p1 = Player(name1)
     p2 = Player(name2)
+
+    # make & shuffle deck
     prepare()
+
+    # play rounds until deck exhausted
     while True:
+        # take cards from top of deck
         p1.take_card(deck)
         p2.take_card(deck)
+        
+        # winner takes loser's cards
         if round_winner(p1.deck[0],p2.deck[0]) == "p1":
             p1.take_card(p2.deck)
         else:
             p2.take_card(p1.deck)
+        
+        # if deck is exhausted
         if len(deck) == 0:
             break
-    return [p1.name,p1.deck,p2.name,len(p2.deck)] if game_winner(p1.deck,p2.deck) == "p1" else [p2.name,p2.deck,p1.name,len(p1.deck)]
+    
+    # return winner and loser information
+    return [p1.name,p1.deck,p2.name,len(p2.deck)] if game_winner(p1.deck,p2.deck) == "p1" else [p2.name,p2.deck,p1.name,len(p1.deck)] # winner name, winner's number of cards, loser name, loser's number of cards
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-import cardGame
+import cardGame,leaderboard
 
+# run game
 results = cardGame.play(input("Please enter the name of player 1: "),input("Please enter the name of player 2: "))
-print(results[0],"won with",len(results[1]),"cards against",str(results[2])+"\'s",results[3])
+print(results[0],"won with",len(results[1]),"cards against",str(results[2])+"\'s",results[3]) # winner name, winner's number of cards, loser name, loser's number of cards
+
+# print cards that winner had
 print(results[0],"had:")
 for i in range(len(results[1])):
-    print(results[1][i][0].capitalize(),results[1][i][1])
+    print(results[1][i][0].capitalize(),results[1][i][1]) # suit, value
+
+# update leaderboard and store top 5
+leaderboard.add(results[0],len(results[1]))
+top_5 = leaderboard.top_5()
+
+for i in range(len(top_5)):
+    print(str(i+1)+".",top_5[i][0],top_5[i][1]) # leaderboard pos, name, score
